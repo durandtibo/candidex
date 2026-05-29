2cfb66e7d7090e2046a8ecd2fa12503bf5d61522
Thibaut Durand <durand.tibo@gmail.com>
2026-05-29 13:20:11 -0700

    Add shared column names

---
+++ src/candidex/columns.py
@@ -0,0 +1,13 @@
+r"""Contain column names."""
+
+from __future__ import annotations
+
+__all__ = ["AUTHORS", "AUTHOR_AFFILIATION", "PAPER_PDF_URL", "PAPER_TITLE", "PAPER_URL"]
+
+
+AUTHORS = "authors"
+AUTHOR_AFFILIATION = "author_affiliation"
+AUTHOR_NAME = "author_name"
+PAPER_PDF_URL = "paper_pdf_url"
+PAPER_TITLE = "paper_title"
+PAPER_URL = "paper_url"
