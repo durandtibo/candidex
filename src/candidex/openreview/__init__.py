bd0f86716de5e28d946c93872d405e77c0f67c6f
Thibaut Durand <durand.tibo@gmail.com>
2026-06-02 09:57:15 -0700

    Rename `search_profiles_by_name`

--- src/candidex/openreview/__init__.py
+++ src/candidex/openreview/__init__.py
@@ -9,7 +9,7 @@ __all__ = [
     "filter_profiles_by_affiliation",
     "filter_profiles_by_email",
     "find_author_profile_ids",
-    "search_openreview_profiles",
+    "search_profiles_by_name",
 ]
 
 from candidex.openreview.client import create_openreview_client
@@ -19,4 +19,4 @@ from candidex.openreview.filtering import (
 )
 from candidex.openreview.matching import do_affiliations_match, does_email_match_domain
 from candidex.openreview.profile import find_author_profile_ids
-from candidex.openreview.search import search_openreview_profiles
+from candidex.openreview.search import search_profiles_by_name
