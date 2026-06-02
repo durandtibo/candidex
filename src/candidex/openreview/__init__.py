4686d1c319182f46474ca6f5a1095c6df2b8ede3
Thibaut Durand <durand.tibo@gmail.com>
2026-06-02 07:18:16 -0700

    Add `do_affiliations_match` (#58)

--- src/candidex/openreview/__init__.py
+++ src/candidex/openreview/__init__.py
@@ -2,7 +2,8 @@ r"""Contain openreview functionalities."""
 
 from __future__ import annotations
 
-__all__ = ["create_openreview_client", "search_openreview_profiles"]
+__all__ = ["create_openreview_client", "do_affiliations_match", "search_openreview_profiles"]
 
 from candidex.openreview.client import create_openreview_client
+from candidex.openreview.matching import do_affiliations_match
 from candidex.openreview.search import search_openreview_profiles
