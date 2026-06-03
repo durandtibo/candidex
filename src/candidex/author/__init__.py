a93ac81eb07acc5ec0ab39880c2d4a61c3fdf27e
Thibaut Durand <durand.tibo@gmail.com>
2026-06-03 13:19:18 -0700

    Add `authors_to_dataframe` (#110)

--- src/candidex/author/__init__.py
+++ src/candidex/author/__init__.py
@@ -2,8 +2,9 @@ r"""Contain utilities for authors."""
 
 from __future__ import annotations
 
-__all__ = ["Author", "deduplicate_authors", "sort_authors"]
+__all__ = ["Author", "authors_to_dataframe", "deduplicate_authors", "sort_authors"]
 
 from candidex.author.author import Author
+from candidex.author.conversion import authors_to_dataframe
 from candidex.author.deduplication import deduplicate_authors
 from candidex.author.sorting import sort_authors
