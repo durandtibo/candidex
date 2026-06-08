a81579fabf2ce1164d220dea73afba8dac5ffb57
Thibaut Durand <durand.tibo@gmail.com>
2026-06-07 21:35:01 -0700

    Add `extract_text_pypdf`

---
+++ src/candidex/pdf/extraction/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain utilities to extract content from digital PDF files."""
+
+from __future__ import annotations
+
+__all__ = ["extract_text_pypdf"]
+
+from candidex.pdf.extraction._pypdf import extract_text_pypdf
