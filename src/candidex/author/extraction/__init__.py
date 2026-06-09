284313f93ed6aab99892334951c90389415c850e
Thibaut Durand <durand.tibo@gmail.com>
2026-06-08 22:06:31 -0700

    Add `BaseAuthorExtractor` (#173)

---
+++ src/candidex/author/extraction/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain code for extracting the authors from a paper."""
+
+from __future__ import annotations
+
+__all__ = ["BaseAuthorExtractor"]
+
+from candidex.author.extraction.base import BaseAuthorExtractor
